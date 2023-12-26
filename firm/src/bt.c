49a718c0c9361fbef2b5e59dffec031b5ae69aa8
Denis Kokarev <d_kokarev@mail.ru>
2023-12-25 23:35:06 -0500

    notifications work too, although simulteoneously, both temp and humidity

--- firm/src/bt.c
+++ firm/src/bt.c
@@ -98,8 +98,8 @@ static struct esp_ctx_s {
 	struct k_work_delayable update_work;
 	uint16_t temp_val;
 	uint16_t hum_val;
-	bool active;
-} esp_ctx = {.temp_val = 20 * 100, .hum_val = 50 * 100, .active = false};
+	int notify_cnt;
+} esp_ctx = {.temp_val = 20 * 100, .hum_val = 50 * 100, .notify_cnt = 0};
 
 static void update_temp() {
 	static double val[THS_SZ];
@@ -125,25 +125,23 @@ static ssize_t read_temp(struct bt_conn *conn, const struct bt_gatt_attr *attr,
 
 #define UPDATE_T (K_MSEC(5 * 1000))
 
-/**
- * Periodic job that updates LED PWM level. Started by led_flow()
- * @param work
- */
-static void update_handler(struct k_work *work) {
-	update_temp();
-	struct k_work_delayable *dwork = k_work_delayable_from_work(work);
-	k_work_reschedule(dwork, UPDATE_T); // re-trigger next invocation
-}
+static void update_handler(struct k_work *work);
 
-
-static void temp_ccc_cfg_changed(const struct bt_gatt_attr *attr,
-                                 uint16_t value) {
-	bool on = (value == BT_GATT_CCC_NOTIFY);
-	if (on) {
+static void reschedule(int upd) {
+	if (!esp_ctx.notify_cnt) {
 		k_work_init_delayable(&esp_ctx.update_work, update_handler);
-	} else {
+		k_work_schedule(&esp_ctx.update_work, UPDATE_T);
+	}
+	esp_ctx.notify_cnt += upd;
+	if (!esp_ctx.notify_cnt) {
 		k_work_cancel_delayable(&esp_ctx.update_work);
 	}
+}
+
+static void temp_ccc_cfg(const struct bt_gatt_attr *attr,
+                         uint16_t value) {
+	int upd = (value == BT_GATT_CCC_NOTIFY) ? +1 : -1;
+	reschedule(upd);
 }
 
 BT_GATT_SERVICE_DEFINE(ess_svc,
@@ -154,13 +152,24 @@ BT_GATT_SERVICE_DEFINE(ess_svc,
                                               BT_GATT_PERM_READ,
                                               read_temp, NULL, &esp_ctx.temp_val),
                        BT_GATT_CUD("Temp", BT_GATT_PERM_READ),
-                       BT_GATT_CCC(temp_ccc_cfg_changed,
+                       BT_GATT_CCC(temp_ccc_cfg,
                                    BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
 
-                       BT_GATT_CHARACTERISTIC(BT_UUID_HUMIDITY, BT_GATT_CHRC_READ,
+                       BT_GATT_CHARACTERISTIC(BT_UUID_HUMIDITY,
+                                              BT_GATT_CHRC_READ | BT_GATT_CHRC_NOTIFY,
                                               BT_GATT_PERM_READ,
                                               read_temp, NULL, &esp_ctx.hum_val),
                        BT_GATT_CUD("Humidity", BT_GATT_PERM_READ),
-                       BT_GATT_CCC(temp_ccc_cfg_changed,
+                       BT_GATT_CCC(temp_ccc_cfg,
                                    BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
 );
+
+static void update_handler(struct k_work *work) {
+	update_temp();
+	struct k_work_delayable *dwork = k_work_delayable_from_work(work);
+	uint16_t v = sys_cpu_to_le16(esp_ctx.temp_val);
+	bt_gatt_notify(NULL, &ess_svc.attrs[2], &v, sizeof(v));
+	v = sys_cpu_to_le16(esp_ctx.hum_val);
+	bt_gatt_notify(NULL, &ess_svc.attrs[6], &v, sizeof(v));
+	k_work_reschedule(dwork, UPDATE_T); // re-trigger next invocation
+}
